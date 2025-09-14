3af0cf28a1f2b6b59db48c87e916e3583d7e0706
KoolShow <51787949+KoolShow@users.noreply.github.com>
2025-09-14 13:56:28 +0800

    3I支持对type处理

--- Lrule/3I.py
+++ Lrule/3I.py
@@ -5,18 +5,44 @@ from minesweepervariants.abs.Lrule import AbstractMinesRule
 from minesweepervariants.abs.board import AbstractBoard, AbstractPosition
 from minesweepervariants.impl.summon.solver import Switch
 from ortools.sat.python.cp_model import CpModel
+import traceback
 
 class Rule3I(AbstractMinesRule):
     name = ["3I", "反相"]
     doc = "染色格中非雷视为雷，雷视为非雷"
+
+    def __init__(self, board: "AbstractBoard" = None, data=None) -> None:
+        super().__init__(board, data)
+
+        board.register_type_special('3I', self.get_type)
+
+        if data == '!':
+            board.set_default_special('3I')
+
 
     def create_constraints(self, board: 'AbstractBoard', switch: 'Switch'):
         model = board.get_model()
         s = switch.get(model, self)
         for key in board.get_interactive_keys():
             for pos, _ in board(key=key):
-                inverted_var = board.get_variable(pos, "3I")
+                inverted_var = board.get_variable(pos, special="3I")
                 if board.get_dyed(pos):
-                    model.Add(inverted_var == board.get_variable(pos).Not()).OnlyEnforceIf(s)
+                    model.Add(inverted_var == board.get_variable(pos, special='raw').Not()).OnlyEnforceIf(s)
                 else:
-                    model.Add(inverted_var == board.get_variable(pos)).OnlyEnforceIf(s)
+                    model.Add(inverted_var == board.get_variable(pos, special='raw')).OnlyEnforceIf(s)
+
+    @staticmethod
+    def get_type(board: 'AbstractBoard', pos: 'AbstractPosition', *args, **kwargs) -> str:
+        s = "".join(traceback.format_stack())
+        if "V.py" not in s:
+            print(s)
+            input()
+        value = board.get_type(pos, special='raw')
+        if board.get_dyed(pos):
+            if value == "C":
+                return "F"
+            elif value == "F":
+                return "N"
+            elif value == "N":
+                return "F"
+        return value
