7d595474b51da23b5f9b48a1a549e225b704aa31
KoolShow <51787949+KoolShow@users.noreply.github.com>
2025-10-26 17:40:22 +0800

    V' & 4S'

---
+++ Lrule/4S'.py
@@ -0,0 +1,38 @@
+"""
+[4S']阶梯': 雷值等于行号与列号之和, 但从0开始
+"""
+from minesweepervariants.abs.Lrule import AbstractMinesRule
+from minesweepervariants.abs.board import AbstractBoard, AbstractPosition
+from minesweepervariants.impl.summon.solver import Switch
+from ortools.sat.python.cp_model import CpModel
+
+class Rule3I(AbstractMinesRule):
+    name = ["4S'", "阶梯'"]
+    doc = "雷值等于行号与列号之和, 但从0开始"
+
+    def __init__(self, board: "AbstractBoard" = None, data=None) -> None:
+        super().__init__(board, data)
+        self.onboard_init(board)
+
+    def onboard_init(self, board: 'AbstractBoard'):
+        board.register_type_special('4S\'', self.get_type)
+
+
+    def create_constraints(self, board: 'AbstractBoard', switch: 'Switch') -> None:
+        model = board.get_model()
+        s = switch.get(model, self)
+        for key in board.get_interactive_keys():
+            for pos, _ in board(key=key):
+                raw = board.get_variable(pos, special='raw')
+                det = board.get_variable(pos, special='4S\'')
+                model.Add(det == pos.x + pos.y).OnlyEnforceIf(raw)
+                model.Add(det == 0).OnlyEnforceIf(raw.Not())
+
+    @staticmethod
+    def get_type(board: 'AbstractBoard', pos: 'AbstractPosition', *args, **kwargs):
+        value = board.get_type(pos, special='raw')
+
+        if value == "F":
+            return pos.x + pos.y
+        else:
+            return 0
---
+++ Rrule/V'.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#
+# @Time    : 2025/06/03 05:26
+# @Author  : Wu_RH
+# @FileName: V.py
+"""
+[V']雷值：每个数字标明周围八格内雷值之和。
+"""
+from ....abs.Rrule import AbstractClueRule, AbstractClueValue
+from ....abs.board import AbstractBoard, AbstractPosition
+
+from ....utils.tool import get_logger
+from ....utils.impl_obj import VALUE_QUESS, MINES_TAG
+
+
+class RuleV(AbstractClueRule):
+    name = ["V'", "雷值", "Value"]
+    doc = "每个数字标明周围八格内雷值之和"
+
+    def __init__(self, board: "AbstractBoard" = None, data=None) -> None:
+        super().__init__(board, data)
+        self.rule = data or "V'"
+
+        class ValueV(AbstractClueValue):
+            def __init__(self, pos: AbstractPosition, count: int = 0, code: bytes = None, rule=self.rule):
+                super().__init__(pos, code)
+                self.rule = rule
+                if code is not None:
+                    # 从字节码解码
+                    self.count = code[0]
+                else:
+                    # 直接初始化
+                    self.count = count
+                self.neighbor = self.pos.neighbors(2)
+
+            def __repr__(self):
+                return f"{self.count}"
+
+            @classmethod
+            def type(cls) -> bytes:
+                return b"V'"
+
+            def code(self) -> bytes:
+                return bytes([self.count])
+
+            def create_constraints(self, board: 'AbstractBoard', switch):
+                """创建CP-SAT约束: 周围雷数等于count"""
+                model = board.get_model()
+
+                # 收集周围格子的布尔变量
+                neighbor_vars = []
+                for neighbor in self.neighbor:  # 8方向相邻格子
+                    if board.in_bounds(neighbor):
+                        var = board.get_variable(neighbor, special=self.rule)
+                        neighbor_vars.append(var)
+
+                # 添加约束：周围雷数等于count
+                if neighbor_vars:
+                    model.Add(sum(neighbor_vars) == self.count).OnlyEnforceIf(switch.get(model, self.pos))
+                    get_logger().warning(f"[V'] Value[{self.pos}: {self.count}] add: {neighbor_vars} == {self.count}")
+
+        self.ValueV = ValueV
+
+
+    def fill(self, board: 'AbstractBoard') -> 'AbstractBoard':
+        logger = get_logger()
+        for pos, _ in board("N", special='raw'):
+            value = board.batch(pos.neighbors(2), "type", special=self.rule)
+            print(value)
+            value = sum(v or 0 for v in value)
+            board.set_value(pos, self.ValueV(pos, count=value, rule=self.rule))
+        return board
