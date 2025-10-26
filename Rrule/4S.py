9092c21f2c49702d385e225e57a07624d26594b7
KoolShow <51787949+KoolShow@users.noreply.github.com>
2025-10-26 20:02:31 +0800

    4S

--- Rrule/4S.py
+++ Rrule/4S.py
@@ -1,70 +1,38 @@
 """
-[4S] 阶梯：所有雷被视为 X 个雷（X 为其的行列数之和）
+[4S]阶梯: 所有雷被视为 X 个雷（X 为其的行列数之和）
 """
+from minesweepervariants.abs.Lrule import AbstractMinesRule
+from minesweepervariants.abs.board import AbstractBoard, AbstractPosition
+from minesweepervariants.impl.summon.solver import Switch
+from ortools.sat.python.cp_model import CpModel
 
-from ....abs.board import AbstractBoard, AbstractPosition
-from ....abs.Rrule import AbstractClueRule, AbstractClueValue
-from ....utils.tool import get_logger
-from ....utils.impl_obj import VALUE_QUESS, MINES_TAG
-
-
-def encode_int_7bit(n: int) -> bytes:
-    # 编码主体：每7位 -> 1字节（bit6~bit0，bit7=0）
-    if n == 0:
-        return b'\x00'
-    payload = []
-
-    while n > 0:
-        payload.append(n & 0x7f)
-        n >>= 7
-
-    return bytes(payload)
-
-
-def decode_bytes_7bit(data: bytes) -> int:
-    if len(data) == 0:
-        return 0
-
-    result = 0
-    for i in data[::-1]:
-        result <<= 7
-        result |= i
-
-    return result
-
-class Rule4S(AbstractClueRule):
+class Rule3I(AbstractMinesRule):
     name = ["4S", "阶梯", "Staircase"]
     doc = "所有雷被视为 X 个雷（X 为其的行列数之和）"
+    lib_only = True
 
-    def fill(self, board: AbstractBoard) -> AbstractBoard:
-        logger = get_logger()
-        for pos, _ in board("N"):
-            positions = pos.neighbors(2)
-            value = sum(p.x + p.y + 2 for p in positions if board.get_type(p) == "F")
-            board.set_value(pos, Value4S(pos, code=encode_int_7bit(value)))
-        return board
+    def __init__(self, board: "AbstractBoard" = None, data=None) -> None:
+        super().__init__(board, data)
+        self.onboard_init(board)
 
-class Value4S(AbstractClueValue):
-    def __init__(self, pos: 'AbstractPosition', code: bytes = b''):
-        super().__init__(pos)
-        self.value = decode_bytes_7bit(code)
-        self.neighbors = pos.neighbors(2)
+    def onboard_init(self, board: 'AbstractBoard'):
+        board.register_type_special('4S', self.get_type)
 
-    def __repr__(self) -> str:
-        return f"{self.value}"
-
-    def high_light(self, board: 'AbstractBoard') -> list['AbstractPosition']:
-        return self.neighbors
-
-    @classmethod
-    def type(cls) -> bytes:
-        return Rule4S.name[0].encode("ascii")
-
-    def code(self) -> bytes:
-        return encode_int_7bit(self.value)
-    
-    def create_constraints(self, board: 'AbstractBoard', switch):
+    def create_constraints(self, board: 'AbstractBoard', switch: 'Switch') -> None:
         model = board.get_model()
         s = switch.get(model, self)
-        var_list = [board.get_variable(pos) * (pos.x + pos.y + 2) for pos in self.neighbors if board.in_bounds(pos)]
-        model.Add(sum(var_list) == self.value).OnlyEnforceIf(s)
+        for key in board.get_interactive_keys():
+            for pos, _ in board(key=key):
+                raw = board.get_variable(pos, special='raw')
+                det = board.get_variable(pos, special='4S')
+                model.Add(det == pos.x + pos.y + 2).OnlyEnforceIf(raw)
+                model.Add(det == 0).OnlyEnforceIf(raw.Not())
+
+    @staticmethod
+    def get_type(board: 'AbstractBoard', pos: 'AbstractPosition', *args, **kwargs):
+        value = board.get_type(pos, special='raw')
+
+        if value == "F":
+            return pos.x + pos.y + 2
+        else:
+            return 0
