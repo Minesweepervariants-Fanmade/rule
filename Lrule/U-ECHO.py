d3c545365e83acaa979a4ad1082f4333352364d4
KoolShow <51787949+KoolShow@users.noreply.github.com>
2026-04-11 21:06:41 +0800

    U-ECHO

---
+++ Lrule/U-ECHO.py
@@ -0,0 +1,23 @@
+"""
+[U-ECHO]回声：抛出参数。
+"""
+
+from ....abs.Lrule import AbstractMinesRule
+from ....abs.board import AbstractBoard
+
+
+class UEchoError(Exception):
+    pass
+
+
+class RuleUECHO(AbstractMinesRule):
+    name = ["U-ECHO", "回声"]
+    doc = "回声：抛出参数。"
+    author = ("NT", 2201963934)
+
+    def __init__(self, board: AbstractBoard, data=None):
+        super().__init__(board, data)
+        raise UEchoError(data)
+
+    def create_constraints(self, board: 'AbstractBoard', switch):
+        return
