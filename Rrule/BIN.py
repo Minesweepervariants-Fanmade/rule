2e8bc55d2f48c4122b50b4111eb3ee907b6a70bd
WuZh-Rh <rhfx0001@gmail.com>
2026-05-11 13:12:45 +0800

    BIN的更多规则兼容

--- Rrule/BIN.py
+++ Rrule/BIN.py
@@ -1,11 +1,14 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 from ....abs.Rrule import AbstractClueRule, AbstractClueValue
 from ....abs.board import AbstractBoard, AbstractPosition
 from ..sharpRule.Csharp import FakeSwitch
+from ....utils.impl_obj import VALUE_QUESS
 from ....utils.tool import get_random
 from ....impl.impl_obj import get_value
 from ....utils.image_create import get_text, get_row
 from ....utils.web_template import MultiNumber
+
+VALUE_SPLIT: bytes = b'BIN_SPLIT_FLAG'
 
 
 class RuleBIN(AbstractClueRule):
@@ -23,7 +26,16 @@ class RuleBIN(AbstractClueRule):
         data_parts = data.split(";")
         if len(data_parts) != 2:
             raise ValueError("BIN rule requires two sub-rules")
-        self.rule = (data_parts[0], data_parts[1])
+
+        self.rules = [
+            board.get_rule_instance(
+                rule_name=self._parse_rule_data(data_parts[0])[0],
+                data=self._parse_rule_data(data_parts[0])[1], add=False
+            ), board.get_rule_instance(
+                rule_name=self._parse_rule_data(data_parts[1])[0],
+                data=self._parse_rule_data(data_parts[1])[1], add=False
+            )
+        ]
 
     def _parse_rule_data(self, rule: str):
         # 解析规则 ID 和 data
@@ -34,56 +46,59 @@ class RuleBIN(AbstractClueRule):
         return rule_id, data
 
     def fill(self, board: AbstractBoard) -> AbstractBoard:
-        rules = [
-            board.get_rule_instance(
-                rule_name=self._parse_rule_data(self.rule[0])[0],
-                data=self._parse_rule_data(self.rule[0])[1]
-            ), board.get_rule_instance(
-                rule_name=self._parse_rule_data(self.rule[1])[0],
-                data=self._parse_rule_data(self.rule[1])[1]
-            )
-        ]
-        if not all(rules):
+        if not all(self.rules):
             raise ValueError("Sub-rules for BIN are not properly defined")
         boards: List[AbstractBoard] = []
-        for rule in rules:
+        for rule in self.rules:
             boards.append(rule.fill(board.clone()))
-        for key in board.get_board_keys():
+        for key in board.get_interactive_keys():
             for pos, _ in board("N", key=key):
                 values = [boards[i].get_value(pos) for i in range(2)]
-                if get_random().randint(0, 1):
-                    values[0], values[1] = values[1], values[0]
-                rule_list: tuple[str, str] = (
-                    values[0].type().decode("ascii"),
-                    values[1].type().decode("ascii")
+                # if get_random().randint(0, 1):
+                #     values[0], values[1] = values[1], values[0]
+                rule_list: tuple[bytes, bytes] = (
+                    values[0].type(),
+                    values[1].type()
                 )
-                value_tuple = (int(values[0].__repr__()), int(values[1].__repr__()))
-                board.set_value(pos, ValueBIN(pos, value_tuple, rule_list, None))
+                code_list: tuple[bytes, bytes] = (
+                    values[0].code(),
+                    values[1].code()
+                )
+                board.set_value(pos, ValueBIN(pos, code_list, rule_list, None))
         return board
 
 
 class ValueBIN(AbstractClueValue):
-    def __init__(self, pos: 'AbstractPosition', value: tuple[int, int] = (0, 0), rule: tuple[str, str] = ("", ""), code: bytes | None = None):
-        super().__init__(pos)
+    def __init__(
+        self, pos: 'AbstractPosition',
+        value: Optional[tuple[bytes, bytes]] = None,
+        rule: Optional[tuple[bytes, bytes]] = None,
+        code: bytes | None = None
+    ):
+        super().__init__(pos, b'')
         if not code:
-            self.value = value
-            self.rule = rule
+            self.value: tuple[bytes, bytes] = value
+            self.rule: tuple[bytes, bytes] = rule
         else:
-            self.value = (code[0], code[1])
-            split_index = code.index(b',')
-            self.rule = (code[2:split_index].decode("ascii"), code[split_index + 1:].decode("ascii"))
+            code_a, code_b, type_a, type_b = code.split(VALUE_SPLIT)
+            self.value: tuple[bytes, bytes] = (code_a, code_b)
+            self.rule: tuple[bytes, bytes] = (type_a, type_b)
 
     def __repr__(self):
-        return f"{self.value[0]} {self.value[1]}"
+        obj1 = self.get_clue(self.value[0], self.rule[0])
+        obj2 = self.get_clue(self.value[1], self.rule[1])
+        return " ".join(sorted([str(obj) for obj in [obj1, obj2]]))
 
     @classmethod
     def type(cls) -> bytes:
         return "BIN".encode("ascii")
 
     def code(self) -> bytes:
-        rule_bytes_1 = self.rule[0].encode("ascii")
-        rule_bytes_2 = self.rule[1].encode("ascii")
-        return bytes([self.value[0], self.value[1]]) + rule_bytes_1 + b',' + rule_bytes_2
+        code_a = self.value[0]
+        code_b = self.value[1]
+        type_a = self.rule[0]
+        type_b = self.rule[1]
+        return VALUE_SPLIT.join([code_a, code_b, type_a, type_b])
 
     def high_light(self, board: AbstractBoard) -> List[AbstractPosition]:
         positions = set()
@@ -97,39 +112,47 @@ class ValueBIN(AbstractClueValue):
         return list(positions)
 
     def compose(self, board) -> Dict:
-        disp = [self.value[0], self.value[1]]
-        disp.sort()
+        obj1 = self.get_clue(self.value[0], self.rule[0])
+        obj2 = self.get_clue(self.value[1], self.rule[1])
         return get_row(
-            get_text(str(disp[0])),
-            get_text(str(disp[1]))
+            obj1.compose(board),
+            obj2.compose(board)
         )
 
     def web_component(self, board) -> Dict:
-        return MultiNumber(sorted([self.value[0], self.value[1]]))
+        return MultiNumber(sorted([
+            str(self.get_clue(self.value[0], self.rule[0])),
+            str(self.get_clue(self.value[1], self.rule[1]))
+        ]))
 
-    def get_clue(self, value: int, rule: str) -> AbstractClueValue:
+    def get_clue(self, value: bytes, rule: bytes) -> Optional[AbstractClueValue]:
         clue_code = bytearray()
-        clue_code.extend(rule.encode("ascii"))
-        clue_code.extend(b'|')
-        clue_code.extend(bytes([value]))
-        return get_value(self.pos, bytes(clue_code))
+        try:
+            clue_code.extend(rule)
+            clue_code.extend(b'|')
+            clue_code.extend(value)
+            return get_value(self.pos, bytes(clue_code))
+        except:
+            return VALUE_QUESS
 
     def create_constraints(self, board: AbstractBoard, switch):
-        model = board.get_model()
-        s = switch.get(model, self)
-
         clue1 = self.get_clue(self.value[0], self.rule[0])
         clue2 = self.get_clue(self.value[1], self.rule[1])
+
         clue3 = self.get_clue(self.value[0], self.rule[1])
         clue4 = self.get_clue(self.value[1], self.rule[0])
+
+        model = board.get_model()
+        s = switch.get(model, self)
 
         select1 = model.new_bool_var(f"BIN_select1_{self.pos}")
         select2 = model.new_bool_var(f"BIN_select2_{self.pos}")
         model.add(select1 == 0).OnlyEnforceIf(s.Not())
         model.add(select2 == 0).OnlyEnforceIf(s.Not())
-        model.add(select1 + select2 == 1).OnlyEnforceIf(s)
+        model.add(sum([select1, select2]) == 1).OnlyEnforceIf(s)
 
         clue1.create_constraints(board, FakeSwitch(select1))
         clue2.create_constraints(board, FakeSwitch(select1))
+
         clue3.create_constraints(board, FakeSwitch(select2))
         clue4.create_constraints(board, FakeSwitch(select2))
