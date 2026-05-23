364bb570c67f0ef8f44b0d63a1c30b0cc7a635a1
KoolShow <51787949+KoolShow@users.noreply.github.com>
2026-05-24 00:37:49 +0800

    fix(gallery): 1B-&1B|

--- sharpRule/gallery.py
+++ sharpRule/gallery.py
@@ -303,11 +303,38 @@ class Rule1B(AbstractMinesRule):
         for key in board.get_interactive_keys():
             boundary_pos = board.boundary(key=key)
 
-            row_positions = board.get_row_pos(boundary_pos)
+            row_positions = board.get_col_pos(boundary_pos)
             row_sums = [
-                sum(board.get_variable(_pos) for _pos in board.get_col_pos(pos))
+                sum(board.get_variable(_pos) for _pos in board.get_row_pos(pos))
                 for pos in row_positions
             ]
             # 所有 row_sums 相等
             for i in range(1, len(row_sums)):
                 model.Add(row_sums[i] == row_sums[0]).OnlyEnforceIf(s1)
+
+class Rule1B_(AbstractMinesRule):
+    id = "1B|"
+    aliases = ("B|",)
+    name = "Column Balance"
+    name.zh_CN = "列平衡"
+    doc = "Each column has the same number of mines"
+    doc.zh_CN = "每列雷数相等"
+    author = ("", 0)
+    tags = ["Untagged"]
+    creation_time = ""
+
+    def create_constraints(self, board: 'AbstractBoard', switch):
+        model = board.get_model()
+        s1 = switch.get(model, self)
+        s2 = switch.get(model, self)
+        for key in board.get_interactive_keys():
+            boundary_pos = board.boundary(key=key)
+
+            col_positions = board.get_col_pos(boundary_pos)
+            col_sums = [
+                sum(board.get_variable(_pos) for _pos in board.get_row_pos(pos))
+                for pos in col_positions
+            ]
+            # 所有 col_sums 相等
+            for i in range(1, len(col_sums)):
+                model.Add(col_sums[i] == col_sums[0]).OnlyEnforceIf(s1)
