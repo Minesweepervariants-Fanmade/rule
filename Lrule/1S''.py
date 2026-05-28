0c4d8f20aeda90dd2efe6df55891dda94af70f3d
WuZh-Rh <rhfx0001@gmail.com>
2026-05-28 22:48:40 +0800

    fix 1S'' 添加1S''的连通性参数

--- Lrule/1S''.py
+++ Lrule/1S''.py
@@ -3,6 +3,7 @@ from ....abs.board import AbstractBoard, AbstractPosition
 
 from .connect import connect
 
+
 class Rule1S(AbstractMinesRule):
     id = "1S''"
     aliases = ("S''",)
@@ -14,34 +15,85 @@ class Rule1S(AbstractMinesRule):
     creation_time = "2025-08-23"
     author = ("", 0)
 
-    def portalNeighbor(self, pos : AbstractPosition, board: AbstractBoard):
+
+    def __init__(self, board: "AbstractBoard" = None, data=None) -> None:
+        super().__init__(board, data)
+        self.nei_values = []
+        if data is None:
+            self.nei_values = [tuple([1])]
+            return
+        nei_values = data.split(";")
+        for nei_value in nei_values:
+            if ":" in nei_value:
+                self.nei_values.append(tuple([
+                    int(nei_value.split(":")[0]),
+                    int(nei_value.split(":")[1])
+                ]))
+            else:
+                self.nei_values.append(tuple([int(nei_value)]))
+
+    def portalNeighbor(self, pos: AbstractPosition, board: AbstractBoard):
         boundary = board.boundary(pos.board_key)
-        neighbors = pos.neighbors(1)
-        result = []
-        for p in neighbors:
-            if board.in_bounds(p):
-                result.append(p)
-        if pos.x == 0:
-            result.append(board.get_pos(boundary.x, pos.y))
-        if pos.x == boundary.x:
-            result.append(board.get_pos(0, pos.y))
-        if pos.y == 0:
-            result.append(board.get_pos(pos.x, boundary.y))
-        if pos.y == boundary.y:
-            result.append(board.get_pos(pos.x, 0))
-        return result
+        positions = []
+        for nei_value in self.nei_values:
+            if len(nei_value) == 1:
+                positions.extend(
+                    pos.neighbors(nei_value[0], nei_value[0])
+                )
+            elif len(nei_value) == 2:
+                positions.extend(
+                    pos.neighbors(nei_value[0], nei_value[1])
+                )
+        return [
+            board.get_pos(
+                pos.row % (boundary.row + 1),
+                pos.col % (boundary.col + 1),
+                pos.board_key
+            ) for pos in positions
+        ]
 
     def create_constraints(self, board, switch):
         model = board.get_model()
         s = switch.get(model, self)
 
-        connect(model, board, s, connect_value=1, nei_value=lambda pos: self.portalNeighbor(pos, board))
+        positions = [(k, p, v) for k in board.get_interactive_keys() for p, v in board(key=k, mode="variable")]
+        n = len(positions)
+        if n < 2:
+            return
+
+        # 边变量：八连通且两端都是雷格
+        arcs, arc_var = [], {}
+        for i, (k1, p1, mv1) in enumerate(positions):
+            va = model.new_bool_var(f"3P_{i}_root")
+            vb = model.new_bool_var(f"3P_root_{i}")
+            arc_var[i, n] = va
+            arc_var[n, i] = vb
+            arcs.append((i, n, va))
+            arcs.append((n, i, vb))
+            model.add(va == 0).OnlyEnforceIf(mv1.Not())
+            model.add(vb == 0).OnlyEnforceIf(mv1.Not())
+            for j, (k2, p2, mv2) in enumerate(positions):
+                if i != j and p2 in self.portalNeighbor(p1, board):
+                    v = model.new_bool_var(f'3P_{i}_{j}')
+                    arc_var[i, j] = v
+                    arcs.append((i, j, v))
+                    model.add(v == 0).OnlyEnforceIf(mv1.Not())
+                    model.add(v == 0).OnlyEnforceIf(mv2.Not())
+
+        # 自环跳过非雷格节点
+        for i, (_, _, mv) in enumerate(positions):
+            arcs.append((i, i, mv.Not()))
+        arcs.append((n, n, False))
+
+        model.add_circuit(arcs).OnlyEnforceIf(s)
 
         tmp_list = []
         for pos, var in board(mode="variable"):
-            tmp_bool = model.NewBoolVar(f"[1S''] {pos}")
+            tmp_bool = model.new_bool_var("tmp")
             var_list = board.batch(self.portalNeighbor(pos, board), mode="variable", drop_none=True)
-            model.Add(sum(var_list) == 1).OnlyEnforceIf([var, tmp_bool, s])
-            model.Add(sum(var_list) == 2).OnlyEnforceIf([var, tmp_bool.Not(), s])
+            model.add(sum(var_list) > 0).OnlyEnforceIf([var, s])
+            model.add(sum(var_list) < 3).OnlyEnforceIf([var, s])
+            model.add(sum(var_list) == 1).OnlyEnforceIf([tmp_bool, s])
+            model.add(var == 1).OnlyEnforceIf([tmp_bool, s])
             tmp_list.append(tmp_bool)
-        model.Add(sum(tmp_list) == 2).OnlyEnforceIf(s)
+        model.add(sum(tmp_list) == 2).OnlyEnforceIf(s)
