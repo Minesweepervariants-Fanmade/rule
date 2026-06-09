64d154fb9cc39e887cc70187cbdc1ecd85a93ab7
WuZh-Rh <rhfx0001@gmail.com>
2026-06-09 13:54:40 +0800

    fix 3A^

--- Rrule/3Ap.py
+++ Rrule/3Ap.py
@@ -5,7 +5,7 @@
 # @Author  : xxx
 # @FileName: 3Ap.py
 from itertools import product
-from typing import List, Dict, Set, Tuple
+from typing import List, Dict, Set, Tuple, Optional, Self
 
 from ortools.sat.python.cp_model import IntVar
 
@@ -76,19 +76,14 @@ class Rule3Ap(AbstractClueRule):
 
 class Value3Ap(AbstractClueValue):
     id = Rule3Ap.id
-    def __init__(self, pos: 'Position', code: bytes = b''):
+
+    def __init__(self, pos: 'Position', value: Optional[int] = None):
         super().__init__(pos)
-        if not code:
-            self.value = 0
-            self.data = []
-            return
-        self.value = code[0] - 128  # 路径异或值
+        self.count = value
+        self.data = []
 
-    def __str__(self) -> str:
-        return f"{self.value}"
-
-    def __repr__(self) -> str:
-        return f"{self.value}"
+    def __repr__(self):
+        return str(self.count)
 
     def high_light(self, board: 'Board') -> List['Position']:
         pos = self.pos.clone()
@@ -135,146 +130,26 @@ class Value3Ap(AbstractClueValue):
         self.data += [-1] * (4 - len(self.data))
 
         # 提取所有首个int值
-        self.value = (self.data[0] ^ self.data[1]) ^ (self.data[2] ^ self.data[3])
-        # get_logger().debug(f"[3A^] {self.pos} {self.data},value {self.value}")
+        self.count = (self.data[0] ^ self.data[1]) ^ (self.data[2] ^ self.data[3])
+        # get_logger().debug(f"[3A^] {self.pos} {self.data},value {self.count}")
 
     @classmethod
-    def type(cls) -> bytes:
-        return Rule3Ap.id.encode("ascii")
+    def from_json(cls, pos: 'Position', data: 'JSONObject') -> 'AbstractValue':
+        _data = deep_unwrap(data)
 
-    def code(self) -> bytes:
-        return bytes([self.value + 128])
+        if not is_value_template(_data):
+            raise TypeError()
 
-    # def bfs_get_states(self, board: 'Board') -> List[dict]:
-    #     """
-    #     大致思路:
-    #     从方向开始走 如果提前走出边境或者循环就直接丢弃 如果刚好在值内就记录
-    #
-    #     一层具象
-    #     从当前位置为根节点构建一棵二叉树进行遍历
-    #     如果说value==-1 那么需要进入while遍历并把所有走出题板的节点删除
-    #     如果value>=1 那么就使用range()
-    #
-    #     二层具象
-    #     如果值为无穷 那么需要使用快慢节点进行遍历 一旦重合就立刻将其值放入list
-    #     可能会遇到重复的pos节点 在遍历的时候需要边查表边遍历
-    #     如果出现过就需要直接赋值而不是继续分叉
-    #
-    #     三层具象
-    #     针对>=1可以使用ab_bfs列表 对当前abfs进行遍历
-    #     如果出现超出题板 那么就丢出
-    #     如果出现循环 不用管 等时间到了他就死了
-    #     每次创建叉的时候需要查看当前经过的路径是否遍历过
-    #
-    #     >=1:
-    #         需要附带的信息有 当前节点, 当前方向, 遍历过的路径的对应值
-    #         具体步骤为:
-    #             沿着当前方向走一步
-    #             查看当前节点有没有超出题板内
-    #                 超出题板就撇了
-    #             查看当前节点在不在对应值列表里面
-    #                 不在就加入对应值列表
-    #             加入b_bfs
-    #     """
-    #
-    #     def move(_pos, _dir):
-    #         match _dir:
-    #             case 0:
-    #                 _pos = _pos.up()
-    #             case 1:
-    #                 _pos = _pos.right()
-    #             case 2:
-    #                 _pos = _pos.down()
-    #             case 3:
-    #                 _pos = _pos.left()
-    #         return _pos
-    #
-    #     def node(_pos, _dt, _value_map):
-    #         result = []
-    #         if _pos in _value_map:
-    #             # 如果在就继续走
-    #             result.append((_pos, (_dt + _value_map[_pos] * 2 + 1) % 4, _value_map.copy()))
-    #         else:
-    #             if (pos_type := board.get_type(_pos)) != "N":
-    #                 # 当前位置是有值的 直接拿了用
-    #                 _value_map = _value_map.copy()
-    #                 _value_map[_pos] = 0 if pos_type == "C" else 1
-    #                 result.append((_pos, (_dt + _value_map[_pos] * 2 + 1) % 4, _value_map))
-    #             else:
-    #                 # 如果不在说明未经过过该节点 就加入map
-    #                 _value_map_a = _value_map.copy()
-    #                 _value_map_b = _value_map.copy()
-    #                 _value_map_a[_pos] = 1
-    #                 _value_map_b[_pos] = 0
-    #                 result.extend([
-    #                     (_pos, (_dt + 3) % 4, _value_map_a),
-    #                     (_pos, (_dt + 1) % 4, _value_map_b),
-    #                 ])
-    #         return result
-    #
-    #     root = self.pos.clone()
-    #     a_bfs = [(root, self.dir, {root.clone(): 0})]
-    #     b_bfs = []
-    #     answer_list = []
-    #
-    #     if self.value == 0:
-    #         flag = False
-    #         # 处理循环的情况
-    #         while len(a_bfs) > 0:
-    #             for pos, dt, value_map in a_bfs:
-    #                 if flag and pos == self.pos and dt == self.dir:
-    #                     # 如果当前位置和方向和自身完全相同 就说明循环了
-    #                     answer_list.append(value_map)
-    #                     continue
-    #                 # 沿着当前方向走一步
-    #                 # 0.上 1.右 2.下 3.左
-    #                 pos = move(pos, dt)
-    #                 if not board.in_bounds(pos):
-    #                     # 查看当前节点有没有超出题板内
-    #                     continue
-    #                 b_bfs.extend(node(pos, dt, value_map))
-    #             a_bfs = b_bfs
-    #             b_bfs = []
-    #             flag = True
-    #         return answer_list
-    #     else:
-    #         for depth in range(self.value, 0, -1):
-    #             for pos, dt, value_map in a_bfs:
-    #                 # 沿着当前方向走一步
-    #                 # 0.上 1.右 2.下 3.左
-    #                 pos = move(pos, dt)
-    #                 # 查看当前节点有没有超出题板内
-    #                 if not board.in_bounds(pos):
-    #                     # 如果depth==self.value就是要的这个结果
-    #                     if depth == 1:
-    #                         answer_list.append(value_map)
-    #                     continue
-    #                 b_bfs.extend(node(pos, dt, value_map))
-    #             a_bfs = b_bfs
-    #             b_bfs = []
-    #         return answer_list
-    #
-    # def deduce_cells(self, board: 'Board') -> bool:
-    #     # 开玩笑 还真能写
-    #     answer_list = self.bfs_get_states(board)
-    #     pos_map = {}
-    #     for line in answer_list:
-    #         for pos in line:
-    #             if pos not in pos_map:
-    #                 pos_map[pos] = [line[pos], 1]
-    #             else:
-    #                 if pos_map[pos][0] == line[pos]:
-    #                     pos_map[pos][1] += 1
-    #     change = False
-    #     for pos in pos_map:
-    #         if pos_map[pos][1] == len(answer_list):
-    #             if board.get_type(pos) == "N":
-    #                 if pos_map[pos][0]:
-    #                     board.set_value(pos, MINES_TAG)
-    #                 else:
-    #                     board.set_value(pos, VALUE_QUESS)
-    #                 change = True
-    #     return change
+        template_data = cast(Template, _data)
+        value = SingleIntValue.try_from(template_data)
+
+        if value is None:
+            raise ValueError()
+
+        return cls(pos, value=value.value)
+
+    def json(self) -> 'JSONObject':
+        return SingleIntValue(self.count).json()
 
     def _enumerate_direction(
         self, board: 'Board',
@@ -392,7 +267,7 @@ class Value3Ap(AbstractClueValue):
         combo_vars = []
         for combo in product(*steps_lists):
             a, b, c, d = combo
-            if ((a ^ b) ^ (c ^ d)) != self.value:
+            if ((a ^ b) ^ (c ^ d)) != self.count:
                 continue
             combo_var = model.NewBoolVar(f"combo_{self.pos}_{a}_{b}_{c}_{d}")
             for idx, step in enumerate(combo):
