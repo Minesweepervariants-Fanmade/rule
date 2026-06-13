ad12fcf17c084837f5ee8bb781708d872a79e9db
WuZh-Rh <rhfx0001@gmail.com>
2026-06-13 09:51:52 +0800

    fix 2E

--- Rrule/2E/2E.py
+++ Rrule/2E/2E.py
@@ -8,18 +8,14 @@
 [2E]加密: 线索被字母所取代，每个字母对应一个线索，且每个线索对应一个字母
 """
 
-from typing import List, Self
+from typing import List, Self, Optional
 
-from minesweepervariants.json_object import JSONObject, deep_unwrap
-from minesweepervariants.utils.value_template import SingleIntValue, SingleValue, is_value_template
-from typing import cast
-from minesweepervariants.abs.rule import AbstractValue
-from minesweepervariants.json_object import deep_unwrap
-from minesweepervariants.utils.value_template import is_value_template, Template, SingleIntValue
-from minesweepervariants.board import JSONObject, Board, Position, Size
-from .....abs.Rrule import AbstractClueRule, AbstractClueValue
-from .....utils.impl_obj import VALUE_QUESS, VALUE_CROSS, VALUE_CIRCLE
-from .....utils.tool import get_random
+from minesweepervariants.utils.value_template import SingleValue
+from minesweepervariants.utils.value_template import SingleIntValue
+from minesweepervariants.board import Board, Position, Size
+from minesweepervariants.abs.Rrule import AbstractClueRule, AbstractClueValue
+from minesweepervariants.utils.impl_obj import VALUE_QUESS, VALUE_CROSS, VALUE_CIRCLE
+from minesweepervariants.utils.tool import get_random
 
 NAME_2E = "2E"
 
@@ -37,14 +33,14 @@ class Rule2E(AbstractClueRule):
     def __init__(self, data=None, board: 'Board' = None):
         super().__init__(board, data)
         pos = board.boundary()
-        size = min(pos.x + 1, 9)
+        size = min(pos.row + 1, 9)
         board.generate_board(NAME_2E, Size(size, size))
         board.set_config(NAME_2E, "pos_label", True)
 
     def fill(self, board: 'Board') -> 'Board':
         self.init_clear(board)
         random = get_random()
-        shuffled_nums = [i for i in range(min(9, board.boundary().x + 1))]
+        shuffled_nums = [i for i in range(min(9, board.boundary().row + 1))]
         random.shuffle(shuffled_nums)
 
         for pos, _ in board("N"):
@@ -52,8 +48,7 @@ class Rule2E(AbstractClueRule):
             if count not in shuffled_nums:
                 board.set_value(pos, VALUE_QUESS)
             else:
-                code = bytes([shuffled_nums[count]])
-                board.set_value(pos, Value2E(pos, code))
+                board.set_value(pos, Value2E(pos, shuffled_nums[count]))
 
         for x, y in enumerate(shuffled_nums):
             pos = board.get_pos(x, y, NAME_2E)
@@ -89,68 +84,50 @@ class Rule2E(AbstractClueRule):
 class Value2E(AbstractClueValue):
     id = Rule2E.id
 
-    def __init__(self, pos: 'Position', code: bytes = b''):
-        value = "ABCDEFGHI"[code[0]]
-        self.value = SingleValue(value)
+    def __init__(self, pos: 'Position', value: int):
+        super(Value2E, self).__init__(pos)
+        self.value: SingleIntValue = SingleIntValue(value)
         self.pos = pos
         self.neighbors = pos.neighbors(2)
+        self.repr: Optional[int] = None
 
+    def __repr__(self):
+        if self.repr:
+            return str(self.repr)
+        return "ABCDEFGHI"[self.value.value]
 
     def web_component(self, board):
-        if not isinstance(self.value, SingleIntValue):
-            assert isinstance(self.value, SingleValue) and isinstance(self.value.value, str)
-            value = "ABCDEFGHI".index(self.value.value)
-            line = board.batch(board.get_col_pos(
-                board.get_pos(0, value, NAME_2E)
-            ), mode="type")
-            if "F" in line:
-                self.value = SingleIntValue(line.index("F"))
-
-        return self.value.web_component()
+        if self.repr:
+            return super().web_component(board)
+        line = board.batch(board.get_col_pos(
+            board.get_pos(0, self.value.value, NAME_2E)
+        ), mode="type")
+        if "F" in line:
+            self.repr = line.index("F")
+        return super().web_component(board)
 
     def compose(self, board):
-        if not isinstance(self.value, SingleIntValue):
-            assert isinstance(self.value, SingleValue) and isinstance(self.value.value, str)
-            value = "ABCDEFGHI".index(self.value.value)
-            line = board.batch(board.get_col_pos(
-                board.get_pos(0, value, NAME_2E)
-            ), mode="type")
-            if "F" in line:
-                self.value = SingleIntValue(line.index("F"))
-
-        return self.value.compose()
-
-    def high_light(self, board: 'Board') -> List['Position']:
-        return self.neighbors
+        if self.repr:
+            return super().compose(board)
+        line = board.batch(board.get_col_pos(
+            board.get_pos(0, self.value.value, NAME_2E)
+        ), mode="type")
+        if "F" in line:
+            self.repr = line.index("F")
+        return super().compose(board)
 
     @classmethod
     def from_json(cls, pos: 'Position', data: 'JSONObject') -> Self:
-        _data = deep_unwrap(data)
+        return cls(pos, data["data"])
 
-        if not is_value_template(_data):
-            raise TypeError()
-
-        if SingleIntValue.try_from(_data):
-            raise ValueError()
-
-        value = SingleValue.try_from(_data)
-
-        if value is None:
-            raise ValueError()
-
-        assert isinstance(value.value, str) and value.value in "ABCDEFGHI"
-
-        return cls(pos, code=bytes([ord(value.value) - ord("A")]))
+    def high_light(self, board: 'Board') -> List['Position']:
+        return self.neighbors
 
     def create_constraints(self, board: 'Board', switch):
         model = board.get_model()
         s = switch.get(model, self)
 
-        assert isinstance(self.value, SingleValue) and not isinstance(self.value, SingleIntValue)
-
-        value = self.value.value
-        assert isinstance(value, str) and value in "ABCDEFGHI"
-        value_index = "ABCDEFGHI".index(value)
+        value_index = self.value.value
 
         line = board.batch(board.get_col_pos(
             board.get_pos(0, value_index, NAME_2E)
